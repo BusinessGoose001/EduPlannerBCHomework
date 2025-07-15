d38fa6ba0917f2b57f4ed27b9a65e64cef068f3f
GillianBryson <gillianfbryson@gmail.com>
2025-07-15 07:30:11 -0700

    add main for API

---
+++ digitcount_api/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"digitcount/digitcount"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+)
+
+type findInSeriesRequest struct {
+	Start     int `json:"series_start"`
+	End       int `json:"series_end"`
+	Increase  int `json:"series_increment"`
+	Type      int `json:"series_type"`
+	SearchKey int `json:"specified_digit"`
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/findInSeries", findInSeries)
+
+	router.Run(":" + getPort())
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	return port
+}
+
+func findInSeries(context *gin.Context) {
+	var request findInSeriesRequest
+
+	if err := context.ShouldBindJSON(&request); err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"error": err})
+		return
+	}
+
+	count, err := digitcount.CountDigitOccurrencesInSeries(request.Start, request.End, request.Increase, request.Type, request.SearchKey)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": err, "count": count})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"count": count})
+
+}
